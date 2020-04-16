3fb8fbcb3560566d578f5683e17531e5d9fe6e61
Raphael Stellwag <r.stellwag@gmx.net>
2020-04-16 23:37:22 +0200

    Version 1.0 Database model

--- README.md
+++ README.md
@@ -2,6 +2,15 @@ docker pull postgres
 
 docker run --rm --name postgres -e POSTGRES_PASSWORD=docker -d -p 5432:5432 -v $HOME/docker/volumes/postgres:/var/lib/postgresql/data postgres
 
+docker kill postgres
+
 psql -h localhost -U postgres -d postgres -f init.sql
 
+
+
+
+docker build -t mypostgres . 
+
 mkdir -p $HOME/docker/volumes/postgres
+
+pgadmin 4
--- init.sql
+++ init.sql
@@ -1,18 +1,23 @@
 SELECT version();
 
---DROP DATABASE meet_together;
+DROP DATABASE meet_together;
 CREATE DATABASE meet_together;
 
 \c meet_together
 
-CREATE TABLE IF NOT EXISTS person (id SERIAL PRIMARY KEY, name TEXT);
+create extension pgcrypto;
 
-CREATE TABLE IF NOT EXISTS event (id SERIAL PRIMARY KEY, name TEXT, description TEXT, accesstoken TEXT, password TEXT, flexible_time BOOLEAN, start_time TIMESTAMP, end_time TIMESTAMP);
+CREATE TABLE IF NOT EXISTS person (id SERIAL PRIMARY KEY, name TEXT, password TEXT, password_generated BOOLEAN);
 
-CREATE TABLE IF NOT EXISTS person_participats_event (person_id SERIAL REFERENCES person (id), event_id SERIAL REFERENCES event (id), creator BOOLEAN, PRIMARY KEY (person_id, event_id));
+-- Add in future maybe column password TEXT for event to increase security -- , start_date TIMESTAMP WITH TIME ZONE, end_date TIMESTAMP WITH TIME ZONE, flexible_time BOOLEAN
+CREATE TABLE IF NOT EXISTS event (id SERIAL PRIMARY KEY, name TEXT, description TEXT, accesstoken TEXT, choosen_time_place INT);
 
-CREATE TABLE IF NOT EXISTS message (id SERIAL PRIMARY KEY, person_id SERIAL REFERENCES person (id), event_id SERIAL REFERENCES event (id), time TIMESTAMP, content TEXT);
+CREATE TABLE IF NOT EXISTS person_interested_in_event (person_id SERIAL REFERENCES person (id), event_id SERIAL REFERENCES event (id), creator BOOLEAN, PRIMARY KEY (person_id, event_id));
 
-CREATE TABLE IF NOT EXISTS time_suggestion (id SERIAL PRIMARY KEY, event_id SERIAL REFERENCES event (id), start_time TIMESTAMP, end_time TIMESTAMP);
+CREATE TABLE IF NOT EXISTS message (id SERIAL PRIMARY KEY, person_id SERIAL REFERENCES person (id), event_id SERIAL REFERENCES event (id), time TIMESTAMP WITH TIME ZONE, content TEXT, generated BOOLEAN, generated_content_description smallint);
 
-CREATE TABLE IF NOT EXISTS can_attend (person_id SERIAL REFERENCES person (id), time_suggestion_id SERIAL REFERENCES time_suggestion (id), PRIMARY KEY (person_id, time_suggestion_id));
+CREATE TABLE IF NOT EXISTS time_place_suggestion (id SERIAL PRIMARY KEY, event_id SERIAL REFERENCES event (id), start_date TIMESTAMP WITH TIME ZONE, end_date TIMESTAMP WITH TIME ZONE, place TEXT, link TEXT);
+
+CREATE TABLE IF NOT EXISTS can_attend (person_id SERIAL REFERENCES person (id), time_place_suggestion_id SERIAL REFERENCES time_place_suggestion (id), PRIMARY KEY (person_id, time_place_suggestion_id));
+
+ALTER TABLE event ADD FOREIGN KEY (choosen_time_place) REFERENCES time_place_suggestion (id)
