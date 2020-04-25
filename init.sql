ae4766f00bb0c5fa1f764f394c1c2f119f777c99
Raphael Stellwag <r.stellwag@gmx.net>
2020-04-25 20:42:53 +0200

    Add left_event and last_read_message

--- init.sql
+++ init.sql
@@ -12,11 +12,11 @@ CREATE TABLE IF NOT EXISTS person (id SERIAL PRIMARY KEY, name TEXT, password TE
 -- Add in future maybe column password TEXT for event to increase security -- , start_date TIMESTAMP WITH TIME ZONE, end_date TIMESTAMP WITH TIME ZONE, flexible_time BOOLEAN
 CREATE TABLE IF NOT EXISTS event (id SERIAL PRIMARY KEY, name TEXT, description TEXT, accesstoken TEXT, choosen_time_place INT);
 
-CREATE TABLE IF NOT EXISTS person_interested_in_event (person_id SERIAL REFERENCES person (id), event_id SERIAL REFERENCES event (id), creator BOOLEAN, PRIMARY KEY (person_id, event_id));
-
-CREATE TYPE generated_content_description As ENUM ('event_created', 'event_updated', 'user_joined_event', 'time_place_suggestion_added', 'time_place_suggestion_choosen');
+CREATE TYPE generated_content_description As ENUM ('event_created', 'event_updated', 'user_joined_event', 'user_left_event', 'time_place_suggestion_added', 'time_place_suggestion_choosen');
 
 CREATE TABLE IF NOT EXISTS message (id SERIAL PRIMARY KEY, person_id SERIAL REFERENCES person (id), event_id SERIAL REFERENCES event (id), time TIMESTAMP WITH TIME ZONE, content TEXT, generated_content_description generated_content_description);
+
+CREATE TABLE IF NOT EXISTS person_interested_in_event (person_id SERIAL REFERENCES person (id), event_id SERIAL REFERENCES event (id), creator BOOLEAN, left_event BOOLEAN default false, last_read_message INT REFERENCES message (id) default null, PRIMARY KEY (person_id, event_id));
 
 CREATE TABLE IF NOT EXISTS time_place_suggestion (id SERIAL PRIMARY KEY, event_id SERIAL REFERENCES event (id), start_date TIMESTAMP WITH TIME ZONE, end_date TIMESTAMP WITH TIME ZONE, place TEXT, link TEXT);
 
